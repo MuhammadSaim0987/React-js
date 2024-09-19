254c05bf558e3f653607b3c2005d6ed33180d2e0
MuhammadSaim0987 <shaikhsaim0987@gmail.com>
2024-09-19 22:50:08 +0530

    New react course

---
+++ practice/TailwindInstallation/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  <h1>Hello world</h1>
+  
+}
+
+export default App
--- practice/src/saim.jsx
+++
@@ -1,7 +0,0 @@
-function Saim(){
-    return(
-        <h1>This is a reactjs project made by Shaikh Muhammad Saim</h1>
-    )
-}
-
-export default Saim
