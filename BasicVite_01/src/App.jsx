28b2d05dc7a3743007508dd45b05937040f38250
MuhammadSaim0987 <shaikhsaim0987@gmail.com>
2024-09-21 10:35:57 +0530

    frist component

---
+++ BasicVite_01/src/App.jsx
@@ -0,0 +1,12 @@
+import Youtube from './Youtube.jsx'
+
+function App() {
+
+return (
+  <>
+    <h1>Hello World again</h1>
+  </>
+  )
+}
+
+export default App
---
+++ BasicVite_01/src/Youtube.jsx
@@ -0,0 +1,8 @@
+function Youtube() {
+
+return(
+    <h1>Starting Youtube...</h1>
+)
+}
+
+export default Youtube
