195e5949b105d5c12d6a3a8920343fa6ec68d34b
MuhammadSaim0987 <shaikhsaim0987@gmail.com>
2024-09-24 22:09:40 +0530

    TaiwindProps

---
+++ 03_TailwindProps/src/App.jsx
@@ -0,0 +1,15 @@
+import Card from './Components/Card'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Card name='Farha naaz' post='Doctor' pic='https://images.unsplash.com/photo-1699205016830-02f55e7b44d8?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG9mZmljZSUyMGxhZHl8ZW58MHx8MHx8fDA%3D'/>
+      <Card name='Alice' post='Cook' pic='https://images.pexels.com/photos/7222982/pexels-photo-7222982.jpeg?auto=compress&cs=tinysrgb&w=800' />
+      <Card name='Alexa' post='Assistant' pic='https://images.pexels.com/photos/3769021/pexels-photo-3769021.jpeg?auto=compress&cs=tinysrgb&w=800'/>
+    </>
+  )
+}
+
+export default App
