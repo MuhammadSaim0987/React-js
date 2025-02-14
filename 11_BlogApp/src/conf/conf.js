35c87df6cb0a01bef71ce842a6f83492a49b67cf
MuhammadSaim0987 <shaikhsaim0987@gmail.com>
2025-02-14 15:56:23 +0530

    Lecture01

---
+++ 11_BlogApp/src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+    appwriteURL : String(process.env.REACT_APPWRITE_URL),
+    appwriteProjectID : String(process.env.REACT_APPWRITE_PROJECT_ID),
+    appwriteDatabaseID : String(process.env.REACT_APPWRITE_DATABASE_ID),
+    appwriteCollectionID : String(process.env.REACT_APPWRITE_COLLECTION_ID),
+    appwriteBucketID : String(process.env.REACT_APPWRITE_BUCKET_ID)
+}
+
+export default conf
